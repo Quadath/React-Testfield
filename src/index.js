dada8b315441118d3c7e09249858e9e4c40729ea
Quadatz <quadath@gmail.com>
2022-02-20 18:01:31 +0200

    events

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 14
+    }
+    // this.nextYear = () => {
+    //   this.setState(state => ({
+    //     years: ++state.years
+    //   }))
+    // }
+
+    //Way 2
+    this.nextYear = this.nextYear.bind(this);
+  }
+  // nextYear = () => {
+  //   this.setState(state => ({
+  //     years: ++state.years
+  //   }))
+  //   // this.state.years++; WRONG
+  // }
+
+  nextYear() {
+    this.setState(state => ({
+      years: ++state.years
+    }))
+    // this.state.years++; WRONG
+  }
+  render() {
+    const {name, surname, link} = this.props;
+    const {years} = this.state;
+    return (
+      <>
+       <button onClick={this.nextYear}>++</button>
+         <h1>My name is {name}, surname - {surname}, years {years} </h1>
+        <a href={link}>My profile</a>
+      </>
+    )
+  }
+}
+
+const All = () => {
+    return (
+      <>
+        <WhoAmI name="Jonh" surname="Smith" link="https://facebook.com"/>
+        <WhoAmI name="Jame" surname="N" link="https://facebook.com"/>
+        <WhoAmI name="Bruh" surname="Smith" link="https://facebook.com"/>
+      </>
+    )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <All/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
